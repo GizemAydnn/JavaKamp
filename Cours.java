70c3a094a876487f9d4aff48b74ee15d077d8d9f
GizemAydnn <82909236+GizemAydnn@users.noreply.github.com>
2021-04-28 05:10:50 +0300

    Add files via upload

---
+++ Cours.java
@@ -0,0 +1,11 @@
+
+public class Cours {
+
+    int coursId;
+    String coursName;
+
+    public Cours(int coursId, String coursName){
+        this.coursId = coursId;
+        this.coursName = coursName;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Category category1 = new Category(1, "Kategori: ","Tümü");
+        Category category2 = new Category(2, "Kategori: ", "Programlama");
+
+        Category[] categories = {category1, category2};
+        for(Category category : categories) {
+            System.out.println(category.categoryName + " + " + category.categoryKind);
+        }
+
+        Cours course1 = new Cours(1, "Java & React kursu");
+        Cours course2 = new Cours(2, "C# & Angular kursu");
+        Cours course3 = new Cours(3, "Programlamaya giriş için temel kurs");
+
+        Cours[] courses = {course1, course2, course3};
+        for(Cours cours : courses){
+            System.out.println("Kurs bilgisi: " + cours.coursId + " + " + cours.coursName);
+        }
+
+        Student student = new Student();
+        student.addToCourse(Cours list);
+
+        Student student1 = new Student();
+        student1.finisesCourse(Cours list1);
+    }
+}

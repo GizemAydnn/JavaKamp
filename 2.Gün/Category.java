f66be180128a59fc855c0f2ab85ff5d078a7fbd9
GizemAydnn <82909236+GizemAydnn@users.noreply.github.com>
2021-06-16 16:10:43 +0300

    Add files via upload

---
+++ 2.Gün/Category.java
@@ -0,0 +1,14 @@
+
+public class Category {
+
+    int categoryId;
+    String categoryKind;
+    String categoryName;
+
+    public Category (int categoryId, String categoryKind, String categoryName){
+        this.categoryId = categoryId;
+        this.categoryKind = categoryKind;
+        this.categoryName = categoryName;
+
+    }
+}
---
+++ 2.Gün/Instructor.java
@@ -0,0 +1,11 @@
+
+public class Instructor {
+
+    String instructorName;
+
+    public Instructor(String instructorName) {
+        this.instructorName = instructorName;
+    }
+}
+
+
